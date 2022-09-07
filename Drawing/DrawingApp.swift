719ac8dce8f62fade11b1bac92ab291fe38e8ed3
Barry Barron <barrybbarron@gmail.com>
2022-09-06 20:57:29 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Barry Barron on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
